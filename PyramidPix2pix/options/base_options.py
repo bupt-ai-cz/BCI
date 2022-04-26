16ab606591f0a02255405fe1565bf6c10dbf466a
shengjieLiu <33684330+SantJay@users.noreply.github.com>
2022-04-26 23:15:59 +0800

    Update base_options.py

--- PyramidPix2pix/options/base_options.py
+++ PyramidPix2pix/options/base_options.py
@@ -42,7 +42,7 @@ class BaseOptions():
         parser.add_argument('--direction', type=str, default='AtoB', help='AtoB or BtoA')
         parser.add_argument('--serial_batches', action='store_true', help='if true, takes images in order to make batches, otherwise takes them randomly')
         parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
-        parser.add_argument('--batch_size', type=int, default=8, help='input batch size')
+        parser.add_argument('--batch_size', type=int, default=2, help='input batch size')
         parser.add_argument('--load_size', type=int, default=320, help='scale images to this size')
         parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
         parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
