d12dfc1d906c71c71f6733ad800c3cbec5d1db05
shengjieLiu <33684330+SantJay@users.noreply.github.com>
2022-04-26 23:18:28 +0800

    Update base_options.py

--- PyramidPix2pix/options/base_options.py
+++ PyramidPix2pix/options/base_options.py
@@ -46,7 +46,7 @@ class BaseOptions():
         parser.add_argument('--load_size', type=int, default=320, help='scale images to this size')
         parser.add_argument('--crop_size', type=int, default=256, help='then crop to this size')
         parser.add_argument('--max_dataset_size', type=int, default=float("inf"), help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
-        parser.add_argument('--preprocess', type=str, default='scale_width_and_crop', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
+        parser.add_argument('--preprocess', type=str, default='none', help='scaling and cropping of images at load time [resize_and_crop | crop | scale_width | scale_width_and_crop | none]')
         parser.add_argument('--no_flip', action='store_true', help='if specified, do not flip the images for data augmentation')
         parser.add_argument('--display_winsize', type=int, default=256, help='display window size for both visdom and HTML')
         # additional parameters
